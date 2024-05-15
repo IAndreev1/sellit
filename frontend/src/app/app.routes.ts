3c37aa5cc93c42b5882694317c5efab2c1773765
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-05-15 15:20:17 +0200

    Homepage is added.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {HomeComponent} from "./components/home/home.component";
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
