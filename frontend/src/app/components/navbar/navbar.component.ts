2a00a1a03bf518de6e83cf061c7b9a362fe1be1e
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-05-15 16:21:52 +0200

    Adding login und register buttons  to navBar.

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(public authService: AuthService,private httpClient: HttpClient,
+              private router: Router) {
+  }
 
+  logoutUser() {
+
+  }
 }
