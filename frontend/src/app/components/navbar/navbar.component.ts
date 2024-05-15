182044fe96ba5c50c2ba55668e658047fde6c714
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-05-15 16:53:52 +0200

    Better looking for login and register components.

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
@@ -19,6 +21,6 @@ export class NavbarComponent {
   }
 
   logoutUser() {
-
+    this.authService.logoutUser();
   }
 }
