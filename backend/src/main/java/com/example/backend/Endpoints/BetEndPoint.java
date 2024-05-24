15695f5e2163be76a727ab6144c23ba039342684
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-05-24 16:17:30 +0200

    feat: Add Controller for bets.

---
+++ backend/src/main/java/com/example/backend/Endpoints/BetEndPoint.java
@@ -0,0 +1,53 @@
+package com.example.backend.Endpoints;
+
+import com.example.backend.Endpoints.Mappers.BetMapper;
+import com.example.backend.Endpoints.dto.BetDto;
+import com.example.backend.Entity.Bet;
+import com.example.backend.service.BetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.invoke.MethodHandles;
+
+@Controller
+@RequestMapping(value = "/api/v1/bet")
+public class BetEndPoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final BetService betService;
+
+    private final BetMapper betMapper;
+
+    public BetEndPoint(BetService betService, BetMapper betMapper) {
+        this.betService = betService;
+        this.betMapper = betMapper;
+    }
+
+    @Secured("ROLE_USER")
+    @PostMapping
+    public BetDto create(BetDto betDto) {
+        LOGGER.trace("create({})", betDto);
+        Bet createdBet = betService.create(betDto);
+        return betMapper.entityToBetDto(createdBet);
+    }
+
+    @Secured("ROLE_USER")
+    @PutMapping
+    public BetDto update(BetDto betDto) {
+        return null;
+    }
+
+    @Secured("ROLE_USER")
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+    }
+
+}
