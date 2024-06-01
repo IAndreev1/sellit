00679552ac8a527fab49e2e47a6fb6e0aaf886e7
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-06-01 16:37:32 +0200

    fix: the product description can be longer.

--- backend/src/main/java/com/example/backend/Entity/Product.java
+++ backend/src/main/java/com/example/backend/Entity/Product.java
@@ -1,5 +1,6 @@
 package com.example.backend.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,11 +19,11 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column
     private String name;
-
+    @Column(columnDefinition = "TEXT")
     private String description;
-
+    @Column
     private Double price;
     @ManyToOne
     @JoinColumn(name = "user_id")
