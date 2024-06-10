d655684f9e2cef0b5848da0cb237f79c39bb115c
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-06-10 10:35:50 +0200

    update: add more notifications.

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -4,15 +4,14 @@ import {
   FormsModule,
   ReactiveFormsModule,
   UntypedFormBuilder,
-  UntypedFormGroup,
   Validators
 } from "@angular/forms";
-import {NavbarComponent} from "../navbar/navbar.component";
-import {AuthService} from "../../services/auth.service";
-import {Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
-import {AuthRequest} from "../../dtos/auth-request";
-import {NgIf} from "@angular/common";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from "@angular/material/snack-bar";
+import { AuthRequest } from "../../dtos/auth-request";
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-login',
@@ -24,39 +23,40 @@ import {NgIf} from "@angular/common";
     NgIf
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   loginForm: FormGroup;
-
   submitted = false;
-
   error = false;
   errorMessage = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-
-  constructor(private formBuilder: UntypedFormBuilder, private authService: AuthService,  private router: Router, private notification:ToastrService) {
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
-
   }
 
   isFormValid(): boolean {
     return this.loginForm.valid;
   }
 
-
   loginUser() {
-
     this.submitted = true;
     const authRequest: AuthRequest = new AuthRequest(this.loginForm.controls.username.value, this.loginForm.controls.password.value);
     this.authenticateUser(authRequest);
   }
 
   /**
-   * Send authentication data to the authService. If the authentication was successfully, the user will be forwarded to the message page
+   * Send authentication data to the authService. If the authentication was successful, the user will be forwarded to the home page.
    *
    * @param authRequest authentication data from the user login form
    */
@@ -66,17 +66,21 @@ export class LoginComponent {
     this.authService.loginUser(authRequest).subscribe({
       next: () => {
         console.log('Successfully logged in user: ' + authRequest.email);
+        this.showNotification("Successfully logged in", "success");
         this.router.navigate(['']);
       },
-      error: error => {
-        let firstBracket = error.error.indexOf('[');
-        let lastBracket = error.error.indexOf(']');
-        let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
-        let errorDescription = error.error.substring(0, firstBracket);
-        errorMessages.forEach(message => {
-          this.notification.error(message, "Could not login user " + authRequest.email);
-        });
+      error: () => {
+        this.showNotification("The email or password is not correct", "error");
       }
+    });
+  }
+
+  showNotification(message: string, type: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: type === "success" ? ['snackbar-success'] : ['snackbar-error']
     });
   }
 }
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -4,16 +4,15 @@ import {
   FormsModule,
   ReactiveFormsModule,
   UntypedFormBuilder,
-  UntypedFormGroup,
   Validators
 } from "@angular/forms";
-import {Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
-import {UserDetail} from "../../dtos/auth-request";
+import { Router } from "@angular/router";
+import { UserDetail } from "../../dtos/auth-request";
 
-import {AuthService} from "../../services/auth.service";
-import {NgIf} from "@angular/common";
-import {NavbarComponent} from "../navbar/navbar.component";
+import { AuthService } from "../../services/auth.service";
+import { NgIf } from "@angular/common";
+import { NavbarComponent } from "../navbar/navbar.component";
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-register',
@@ -23,24 +22,29 @@ import {NavbarComponent} from "../navbar/navbar.component";
     ReactiveFormsModule,
     NgIf,
     NavbarComponent,
-
   ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
   registerForm: FormGroup;
   submitted = false;
   error = false;
   errorMessage = '';
-
-  constructor(private formBuilder: UntypedFormBuilder, private authService: AuthService, private router: Router, private notification: ToastrService) {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
     this.registerForm = this.formBuilder.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      email: ['', [Validators.required,Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
-      password2 : ['', [Validators.required, Validators.minLength(8)]]
+      password2: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
 
@@ -51,15 +55,22 @@ export class RegisterComponent {
   registerUser() {
     this.submitted = true;
 
-    if(this.registerForm.controls.password.value != this.registerForm.controls.password2.value) {
-      this.notification.error("Passwords don't match!")
+    if (this.registerForm.controls.password.value !== this.registerForm.controls.password2.value) {
+      this.showNotification("Passwords don't match!", "error");
     } else {
-      const userDetail: UserDetail = new UserDetail(null,this.registerForm.controls.firstName.value,this.registerForm.controls.lastName.value,  this.registerForm.controls.email.value, this.registerForm.controls.password.value,null);
-      console.log(userDetail)
+      const userDetail: UserDetail = new UserDetail(
+        null,
+        this.registerForm.controls.firstName.value,
+        this.registerForm.controls.lastName.value,
+        this.registerForm.controls.email.value,
+        this.registerForm.controls.password.value,
+        null
+      );
+      console.log(userDetail);
       this.authService.registerUser(userDetail).subscribe({
         next: () => {
           console.log('Successfully registered user: ' + userDetail.email);
-          this.notification.success( "Successfully registered user:  " + userDetail.email)
+          this.showNotification("Successfully registered user: " + userDetail.email, "success");
           this.router.navigate(['']);
         },
         error: error => {
@@ -69,14 +80,20 @@ export class RegisterComponent {
           let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
           let errorDescription = error.error.substring(0, firstBracket);
           errorMessages.forEach(message => {
-            this.notification.error(message, "Could not register user " + userDetail.email);
+            this.showNotification(message, "error");
           });
         }
       });
     }
+  }
 
-
-
+  showNotification(message: string, type: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: type === "success" ? ['snackbar-success'] : ['snackbar-error']
+    });
   }
 
   vanishError() {
